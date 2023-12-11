e57e108e7768e31736cd373aa640f192ca0ac6d7
Milad Joodi <winbeta.com@gmail.com>
2023-12-11 19:01:59 +0330

    Initial commit

---
+++ 01/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div className="add-course"></div>
+      <div className="list"></div>
+    </div>
+  );
+}
+
+export default App;
