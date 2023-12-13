33ba59b2a97252d360f644c8c849219a434245eb
Milad Joodi <winbeta.com@gmail.com>
2023-12-13 18:17:05 +0330

    ♥

--- 10/src/App.js
+++ 10/src/App.js
@@ -1,8 +1,22 @@
+import React, {useState} from 'react'
 import "./App.css";
-import SubmitForm from "./Components/submitForm";
+import {useToggle} from './useToggle';
+
 function App() {
-  return <div className="App">
-  <SubmitForm />
-  </div>;
+    const [isVisible, toggle] = useToggle()
+    const [isVisible2, toggle2] = useToggle()
+
+  return( <div className="App">
+    <button onClick={toggle}>
+      {isVisible ? "Hide" : "Show"}
+    </button>
+  {isVisible && <h1>This is my hidden text</h1>}
+
+  <button onClick={toggle2}>
+      {isVisible2 ? "Hide" : "Show"}
+    </button>
+  {isVisible && <h1>This is my hidden text</h1>}
+
+  </div>);
 }
 export default App;
