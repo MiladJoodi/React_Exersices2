9c5f2e925b6b06d873cf58813efc6affa0167889
Milad Joodi <winbeta.com@gmail.com>
2023-12-14 14:30:00 +0330

    ♥

---
+++ 12.UseReducer_FetchCatFact/src/factReducer.js
@@ -0,0 +1,28 @@
+import { ACTION_TYPES } from "./factAction"
+
+export const initialState = {
+  loading: false,
+  fact: "",
+  error: false
+}
+
+// Fetch_start   : loading :true
+// fetch_success : loading: false
+//               : fact : res.data.fact
+//
+// fetch_error   : loading: false
+//    
+
+export const factReducer = (state, action)=>{
+
+  switch (action.type) {
+    case ACTION_TYPES.Fetch_start:
+      return {loading: true, fact: "", error: false}
+    case ACTION_TYPES.fetch_success:
+      return {loading: false, fact: action.data, error: false}
+    case ACTION_TYPES.fetch_error:
+      return {loading: false, fact: "", error: true}
+    default:
+      return state;
+  }
+}
